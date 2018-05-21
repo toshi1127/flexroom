59dc11127a5e0da936165e5d3e40f431497e1608
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-22 02:53:51 +0900

    delete console.log

--- server.js
+++ server.js
@@ -1,201 +1,24 @@
-var port = process.env.PORT || 9001;
+const app = require('express')();
+var express = require('express');
+const http = require('http').Server(app);
+const path = require("path");
+const ejs = require('ejs');
+const io = require('socket.io')(http);
 
-var server = require('http'),
-    url = require('url'),
-    path = require('path'),
-    fs = require('fs'),
-    ejs = require('ejs'),
-    io = require('socket.io');
-const hostname = '10.110.131.224';
-var app;
-var options = {
-    key: fs.readFileSync(path.join(__dirname, 'fake-keys/privatekey.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'fake-keys/certificate.pem'))
-};
-app = server.createServer();
-app.on('request', function (request, response) {
-    switch (request.url) {
-        case '/'://カレントディレクトリは/で読んでしまう　故にディレクトリを分けて管理する必要がある。
-            fs.readFile(path.join(process.cwd(), 'index.html'),
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/css/bootstrap.min.css':
-            fs.readFile('./css/bootstrap.min.css', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/css' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/css/bootstrap.css':
-            fs.readFile('./css/bootstrap.css', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/css' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/css/client.css':
-            fs.readFile('./css/client.css', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/css' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/css/client2.css':
-            fs.readFile('./css/client.css', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/css' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/css/iziToast.css':
-            fs.readFile('./css/iziToast.css', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/css' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/css/font-awesome.min.css':
-            fs.readFile('./css/font-awesome.min.css', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/css' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/img/Night-Restaurant-Cebu-UHD-Wall-Paper.jpg':
-            fs.readFile('./img/Night-Restaurant-Cebu-UHD-Wall-Paper.jpg', 'binary',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'image/jpg' });
-                    response.write(data, 'binary');
-                    response.end();
-                }
-            );
-            break;
-        case '/img/check.png':
-            fs.readFile('./img/check.png', 'binary',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'image/png' });
-                    response.write(data, 'binary');
-                    response.end();
-                }
-            );
-            break;
-        case '/img/error.png':
-            fs.readFile('./img/error.png', 'binary',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'image/png' });
-                    response.write(data, 'binary');
-                    response.end();
-                }
-            );
-            break;
-        case '/img/info.png':
-            fs.readFile('./img/info.png', 'binary',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'image/png' });
-                    response.write(data, 'binary');
-                    response.end();
-                }
-            );
-            break;
-        case '/img/error.png':
-            fs.readFile('./img/error.png', 'binary',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'image/png' });
-                    response.write(data, 'binary');
-                    response.end();
-                }
-            );
-            break;
-        case '/js/iziToast.js':
-            fs.readFile('./js/iziToast.js', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/js' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/js/webrtc.js':
-            fs.readFile('./js/webrtc.js', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/js' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/js/main.js':
-            fs.readFile('./js/main.js', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/js' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/js/getScreenId.js':
-            fs.readFile('./js/getScreenId.js', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/js' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/fonts/glyphicons-halflings-regular.ttf':
-            fs.readFile('./fonts/glyphicons-halflings-regular.ttf', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'application/x-font-ttf' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/fonts/glyphicons-halflings-regular.woff':
-            fs.readFile('./fonts/glyphicons-halflings-regular.woff', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'application/x-font-woff' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-        case '/html/help.html':
-            fs.readFile('./html/help.html', 'UTF-8',
-                function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.write(data);
-                    response.end();
-                }
-            );
-            break;
-    }
+var index = require('./routes/index');
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+
+var port = '8080';
+http.listen(process.env.PORT || port, () => {
+
 });
-function runServer() {
-    app = app.listen(process.env.PORT || port, function () {
-        console.log('Server listening at https://' + ':' + process.env.PORT);
-        console.log(process.env.IP);
-    });
-}
-
-runServer();
 
 var myObj3 = new Object();
 var myObj5 = new Object();
@@ -203,7 +26,6 @@ var myObj6 = new Object();
 var myObj7 = new Object();
 var myObj2 = new Object();
 var myObj4 = new Object();
-var wsServer = io.listen(app);
 
 var pear = null;
 var pear2 = null;//追加
@@ -212,7 +34,7 @@ function getUniqueStr(myStrong) {
     if (myStrong) strong = myStrong;
     return new Date().getTime().toString(16) + Math.floor(strong * Math.random()).toString(16)
 }
-wsServer.on('connection', function (ws) {//途中からアクセスした場合
+io.on('connection', function (ws) {//途中からアクセスした場合
     console.log('-- websocket connected --');
     function setRoomname(room) {
         ws.roomname = room;
@@ -232,11 +54,7 @@ wsServer.on('connection', function (ws) {//途中からアクセスした場合
     ws.on('message', function (message) {
         var Message = JSON.parse(message);
         if (Message.type === "join") {
-            console.log("create list");
-            console.log(Message.id + "さんです。");
-            //部屋が違ったら登録おkにする
             if (myObj7[myObj6[myObj5[Message.id]]] === Message.room) {
-                console.log("重複しています");
                 const errormessage = JSON.stringify({
                     type: 'error',
                     text: Message.id + "は登録されています",
@@ -253,7 +71,6 @@ wsServer.on('connection', function (ws) {//途中からアクセスした場合
             else {
                 var ID = getUniqueStr(Message.id);
                 ws.join(Message.room);
-                console.log('id=' + ws.id + 'enter room=' + Message.room);
                 setRoomname(Message.room);
                 myObj3[ID] = Message.id;
                 myObj5[Message.id] = ID;
@@ -267,19 +84,13 @@ wsServer.on('connection', function (ws) {//途中からアクセスした場合
                     text: ID
                 });
                 ws.send(sendid);
-                console.log("ログイン通知を送りました。");
                 for (var key in myObj3) {//グループから一人を摘出
-                    console.log("ルーム名:" + ws.roomname);
-                    console.log("ループ中");
-                    console.log(key);
                     if (myObj7[myObj6[key]] === ws.roomname) {
-                        console.log("ルームメンバー:" + myObj3[key]);
                         const message = JSON.stringify({
                             type: 'name',
                             text: myObj3[key],
                             fromid: key,
                         });
-                        //http://thr3a.hatenablog.com/entry/20141123/1416723018
                         for (var Key in myObj3) {
                             if (myObj7[myObj6[Key]] === ws.roomname) {
                                 ws.to(myObj6[Key]).emit('message', message);
@@ -302,11 +113,9 @@ wsServer.on('connection', function (ws) {//途中からアクセスした場合
             //ここで接続先の名前からIDを求める
             if (Message.id in myObj5) {
                 if (pear === null && pear2 === null) {//追加
-                    console.log("offer answer=" + Message.id);
                     pear = myObj5[Message.id];//ここがanswe→offerになってそのまま。
                 }
                 else if (pear != null && pear2 === null) {//追加
-                    console.log("answer offer=" + Message.id);
                     pear2 = myObj5[Message.id];//ここがanswe→offerになってそのまま。追加
                     //pear2にはtoshiが入ってる！！
                 }
@@ -323,9 +132,7 @@ wsServer.on('connection', function (ws) {//途中からアクセスした場合
                 }
             }
         }
-        else if (Message.type === "logout") {//自分以外の全員に送っている。普段も同じ
-            //wsに関する情報を全て削除する。
-            console.log("ログアウトします。");
+        else if (Message.type === "logout") {
             sendmessage(message);
             ws.disconnect();
             delete myObj5[myObj3[myObj2[ws.id]]];
